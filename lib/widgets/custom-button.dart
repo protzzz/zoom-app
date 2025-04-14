cb03030ec701b5d6ff6c434e961c690635d8c40c
Bohdan <protzzz.it@gmail.com>
2025-04-14 23:00:00 +0200

    updated CustomButton view

--- lib/widgets/custom-button.dart
+++ lib/widgets/custom-button.dart
@@ -1,14 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:zoom_app/utils/colors.dart';
 
 class CustomButton extends StatelessWidget {
   final String text;
-  const CustomButton({super.key, required this.text});
+  final VoidCallback onPressed;
+  const CustomButton({
+    super.key,
+    required this.text,
+    required this.onPressed,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: () {},
-      child: Text(text, style: TextStyle(fontSize: 18)),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 90),
+      child: ElevatedButton(
+        onPressed: onPressed,
+        style: ElevatedButton.styleFrom(
+          backgroundColor: buttonColor,
+          minimumSize: Size(double.infinity, 50),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(30),
+            side: BorderSide(color: buttonColor),
+          ),
+        ),
+        child: Text(
+          text,
+          style: TextStyle(fontSize: 18, color: Colors.white),
+        ),
+      ),
     );
   }
 }
