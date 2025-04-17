f649a860559a84735239835884c2949b7eb59eac
Bohdan <protzzz.it@gmail.com>
2025-04-17 19:09:29 +0200

    auto added dependencies to work with web services

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:zoom_app/pages/home.dart';
 import 'package:zoom_app/pages/login.dart';
 import 'package:zoom_app/utils/colors.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
